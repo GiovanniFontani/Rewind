1ed065214a535c3c34d4579ee1f4f67de7497cbb
Leonardo Paroli <postaperleonardo@gmail.com>
2022-02-06 17:08:33 +0100

    Bookmark DB and Sounds/Anim
    Refined database interactions
    Added sounds and animations libraries and functionalities
    Added bookmark recycler view row selection
    Pending bookmark delete interaction

--- app/src/main/java/com/example/rewind/bookmarking/BookmarkListAdapter.java
+++ app/src/main/java/com/example/rewind/bookmarking/BookmarkListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.rewind.bookmarking;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.ListAdapter;
 import com.example.rewind.bookmarking.database.Bookmark;
 
 public class BookmarkListAdapter extends ListAdapter<Bookmark, BookmarkViewHolder> {
+    int selectedPosition=-1;
     public BookmarkListAdapter(@NonNull DiffUtil.ItemCallback<Bookmark> diffCallback) {
         super(diffCallback);
     }
@@ -19,10 +22,20 @@ public class BookmarkListAdapter extends ListAdapter<Bookmark, BookmarkViewHolde
         return BookmarkViewHolder.create(parent);
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onBindViewHolder(BookmarkViewHolder holder, int position) {
         Bookmark current = getItem(position);
         holder.bind(current.name,current.documentName,current.date, Integer.toString(current.bk_id));
+        if(selectedPosition == position) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#000000"));
+        }else{
+            holder.itemView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        holder.itemView.setOnClickListener(v -> {
+            selectedPosition=holder.getAbsoluteAdapterPosition();
+            notifyDataSetChanged();
+        });
     }
 
     static class BookmarkDiff extends DiffUtil.ItemCallback<Bookmark> {
